b52c4b66ae0e9316c3ab61c5cab1209216543245
Deepayan137 <deepayan137@gmail.com>
2018-05-31 17:09:07 +0530

    masking functionalities added

--- src/main.py
+++ src/main.py
@@ -5,19 +5,43 @@ import pdb
 from PIL import Image
 import cv2
 import models.VGG_FACE
+
+from src.utils import apply_mask, get_gaussian_mask, KL_divergence
 # import torchvision.models as models
 
 model = models.VGG_FACE.VGG_FACE
 model.load_state_dict(torch.load('models/VGG_FACE.pth'))
 
 path = 'data/'
-def evaluate(image):
-	model.eval()
-	out = model(image)
-	_, predicted = torch.max(out.data, 1)
-	return predicted.data.cpu().numpy()[0]
+
+# print(model)
+def toNumpy(tensor):
+	return tensor.data.cpu().numpy()[0]
+
 def toTensor(image):
 	return torch.Tensor(image)
+
+def predict(probs):
+	_, predicted = torch.max(probs.data, 1)
+	return toNumpy(predicted)
+
+def evaluate(image, layer=None):
+	k = 0
+	model.eval()
+	if layer is not None:
+		k = layer
+	probs = model[k:](image)
+	return probs
+	
+def get_activation(image, layer=None):
+
+	def take_transpose(activations):
+		return torch.squeeze(activations).permute(1 ,2, 0)
+	
+	if layer is not None:
+		return take_transpose(model[:layer](image))
+	return take_transpose(model(image))
+
 
 def get_class_names(lookup_file):
 	with open(lookup_file, 'r') as in_file:
@@ -31,6 +55,12 @@ def get_idx(classes):
 		name_to_idx[class_] = len(name_to_idx)
 	return name_to_idx
 
+def image_prep(image):
+	im = np.array(image).astype(np.float32)
+	im = im.transpose((2,0,1))
+	im = np.expand_dims(im, axis=0)
+	im = toTensor(im)
+	return im
 
 if __name__ == '__main__':
 	filepath = os.path.join(path, 'files')
@@ -38,16 +68,34 @@ if __name__ == '__main__':
 	classes = get_class_names(lookup_file)
 	name_to_idx = get_idx(classes)
 	idx_to_name = dict(zip(name_to_idx.values(), name_to_idx.keys()))
-	image_file = 'data/faces/Adam_Hicks/9.jpg'
+	image_file = 'data/faces/Nina_Arianda/8.jpg'
 	im = cv2.imread(image_file)
 	dim = (224, 224)
 	im = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
-	im = np.array(im).astype(np.float32)
-	im = im.transpose((2,0,1))
-	im = np.expand_dims(im, axis=0)
-	im = toTensor(im)
-	predicted = evaluate(im)
-	print(idx_to_name[int(predicted)])
+	mx, my = im.shape[0]//2, im.shape[1]//2
+	im_temp = im
+	im = image_prep(im)
+	probs_true = evaluate(im)
+	activation = get_activation(im, 17)
+	activation = activation.data.cpu().numpy()
+	# print(activation.shape)
+	masked_image, mask = apply_mask(activation, mx=mx, my=my, sigma=30, mask=None)
+	masked_image = image_prep(masked_image)
+	probs = evaluate(masked_image, layer=17)
+	p_scores = {
+				'probs_true': probs_true,
+				'probs': probs
+				}
+	for key in p_scores:
+		p_scores[key] = toNumpy(p_scores[key])
+	# pdb.set_trace()
+	print(KL_divergence(p_scores['probs_true'], p_scores['probs']))
+	mask_reshaped = cv2.resize(mask, dim, interpolation = cv2.INTER_AREA)
+	masked_image, mask = apply_mask(im_temp, mask=mask_reshaped)
+	cv2.imwrite('masked.jpg', masked_image)
+	pdb.set_trace()
+	# print(idx_to_name[int(new_predict)])
+	# print(idx_to_name[int(predicted)])
 	
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import cv2
 import pdb
 import numpy as np 
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+from scipy.stats import entropy
 def gaussian(**kwargs):
 
 	return np.exp(-4*np.log(2) * ((kwargs['x'] - kwargs['mu_x'])**2 + \
 				 (kwargs['y'] - kwargs['mu_y'])**2) / kwargs['sigma']**2)
 
-def get_Gaussian_mask(image, mx, my, sigma):
+def get_gaussian_mask(image, mx, my, sigma):
 	"""
 	The given fuction takes cordinates mx and my 
 	and returns a matrix the size of image .
@@ -25,20 +26,23 @@ def get_Gaussian_mask(image, mx, my, sigma):
 		
 	return mask
 
-def apply_mask(image, mask):
+def apply_mask(image,**kwargs):
 	"""takes the gaussian mask and applies it on the image"""
-
+	mask = kwargs['mask']
+	if kwargs['mask'] is None:
+		mask = get_gaussian_mask(image, kwargs['mx'], kwargs['my'], kwargs['sigma'])
 	for ch in range(image.shape[2]):
 		image[:, :, ch] = np.multiply(image[:, :, ch], mask)
-	return image
+	return image, mask
 
-path = 'cat.jpg'
-if __name__ == '__main__':
-	image = cv2.imread(path)
-	plt.axis("off")
-	plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-	mx, my = image.shape[0]//2, image.shape[1]//2
-	mask = get_Gaussian_mask(image, mx, my, 300)
-	masked_image = apply_mask(image, mask)
-	plt.imshow(cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB))
-	plt.show() 	
+def KL_divergence(probs_true, probs):
+	return entropy(probs_true, probs)
+# path = 'cat.jpg'
+# if __name__ == '__main__':
+# 	image = cv2.imread(path)
+# 	plt.axis("off")
+# 	# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+# 	mx, my = image.shape[0]//2, image.shape[1]//2
+# 	masked_image = apply_mask(image, mx, my, 300)
+# 	plt.imshow(cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB))
+# 	plt.show() 	
